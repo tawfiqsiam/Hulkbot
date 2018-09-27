62356db196f875e939eda390c70f0cfb5b5f3eef
FHGDev <gideon.lingle14@gmail.com>
2018-09-27 17:08:56 -0500

    Update clearlogs.js

--- commands/clearlogs.js
+++ commands/clearlogs.js
@@ -2,8 +2,9 @@ const config = require('../json/config.json')
 const prefix = process.env.prefix
 
 module.exports.run = (bot, message, args, discord) => {
+  if (!config.owners.includes(message.author.id)) return message.channel.send("Nope!")
   if (message.channel.id == "356181785899565077") {
-      message.channel.bulkDelete(100).catch(console.err)
+      message.channel.bulkDelete(100).catch(console.error)
       message.channel.send("Successfully cleared the logs.").then(m => m.delete(5000))
     } else {
         console.log("Failed to clear logs. :-(")
