2b9bba579c89df56289db27bd478093c24fb468e
FHGDev <gideon.lingle14@gmail.com>
2018-10-09 14:26:56 -0500

    Create volume.js

---
+++ commands/volume.js
@@ -0,0 +1,16 @@
+module.exports.run = (bot, message, args) = {
+  const vc = message.member.voiceChannel
+  if (!vc) return;
+  vc.join().then(conn => {
+    conn.setVolume(args.join(" "))
+    if (args.join(" ") > 200) {
+      message.channel.send("You can't set the volume to over 200.")
+    } else {
+      message.channel.send(`I set my volume to ${args.join(" ")}`)
+    }
+  })
+}
+
+module.exports.help = {
+  name: "setvol"
+}
