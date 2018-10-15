a94e7fe78a993708f767ebb42a012f5326a90be4
FHGDev <gideon.lingle1@gmail.com>
2018-10-15 12:35:38 -0500

    Update shorten.js

--- commands/shorten.js
+++ commands/shorten.js
@@ -6,15 +6,20 @@ const discord = require('discord.js')
 
 module.exports.run = (bot, message, args) => {
   const link = args.join(" ");
-  var l;
   api.shorten(link, (err, li) => {
-    l = li;
+    const em = new discord.RichEmbed()
+    .addField("Link Shortener", `I shortened your link for you. Here's your link!\n${l}`)
+    .setTimestamp()
+    .setColor("RANDOM")
+    message.channel.send({embed: em})
+    if (err) {
+      const emb = new discord.RichEmbed()
+      .addField("Link Shortener", `I couldn't shorten your url...`)
+      .setTimestamp()
+      .setColor("RANDOM")
+      message.channel.send({embed: emb})
+    }
   }) 
-  const em = new discord.RichEmbed()
-  .addField("Link Shortener", `I shortened your link for you. Here's your link!\n${l}`)
-  .setTimestamp()
-  .setColor("RANDOM")
-  message.channel.send({embed: em})
 }
 
 module.exports.help = {
