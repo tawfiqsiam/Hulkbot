6cce30e18ea3fa6f696bb2dbfc02bf7003ba83ff
FHGDev <gideon.lingle1@gmail.com>
2018-10-15 12:30:39 -0500

    Create shorten.js

---
+++ commands/shorten.js
@@ -0,0 +1,22 @@
+const url = require('google-url')
+const api = new url({key: process.env.key})
+const discord = require('discord.js')
+
+"use strict";
+
+module.exports.run = (bot, message, args) => {
+  const link = args.join(" ");
+  var l;
+  api.shorten(link, (err, link) => {
+    l = link;
+  }) 
+  const em = new discord.RichEmbed()
+  .addField("Link Shortener", `I shortened your link for you. Here's your link!\n${l}`)
+  .setTimestamp()
+  .setColor("RANDOM")
+  message.channel.send({embed: em})
+}
+
+module.exports.help = {
+  name: "shorten"
+}
