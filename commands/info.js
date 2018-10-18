4f6f0456b25639064cbc9cd8be06a15dc675549c
FHGDev <gideon.lingle1@gmail.com>
2018-10-18 10:35:52 -0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,14 +1,15 @@
 const pkg = require('../package.json')
 
-module.exports.run = (bot, message, args) => {
-  var msgArray = []; 
-  msgArray.push(`Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot running on NodeJS Version 8.`);
-  msgArray.push(`My create date is January of 2017`)
-  msgArray.push(`I'm currently running on Discord.js version 12.0.0 which utilizes the latest version of Discord.js, an *unofficial* Discord libary by hydrabolt.`);
-  msgArray.push(`The developers of my source code are <@242734840829575169> and <@140487710727995392>.`);
-  msgArray.push(`To see what I can do, use h!help`);
-  msgArray.push(`My invocation method is using prefixes, currently, I only respond to messages beginning with h!`);
-  message.channel.send(msgArray)
+module.exports.run = (bot, message, args, discord) => {
+  const embed = new discord.RichEmbed()  
+  .addField('Hi!' `Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot running on NodeJS Version 8.`)
+  .addField(`When was I born?`,`My create date is January of 2017`)
+  .addField(`Version?`,`I'm currently running on Discord.js version 12.0.0 which utilizes the latest version of Discord.js, an *unofficial* Discord library by hydrabolt.`)
+  .addField(`Creators`,`The developers of my source code are <@242734840829575169> and <@140487710727995392>.`)
+  .addField(`How to use me`,`To see what I can do, use h!help`)
+  .addField(`More!`,`My invocation method is using prefixes, currently, I only respond to messages beginning with h!`)
+  .setTimestamp()
+  message.channel.send({embed: embed})
 }
 
 module.exports.help = {
