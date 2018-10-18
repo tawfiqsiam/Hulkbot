9cd4cdbafe343a58a800466e6681eabc5ee44d05
FHGDev <gideon.lingle1@gmail.com>
2018-10-18 10:48:24 -0500

    Update consoles.js

--- util/consoles.js
+++ util/consoles.js
@@ -1,7 +1,7 @@
 module.exports = (bot, config) => {
-  console.log("Bot is started. Get ready for some sweet commands!");
-  console.log("Bot name: " + bot.user.username + `\n`);
-  console.log("Bot owner: " + config.owner + `\n`);
-  console.log(`Bot Developers: ${config.owner}, ${config.dev2}, and ${config.dev3}\n`);
+  console.log("Bot is started. Get ready for some sweet commands!\n\n");
+  console.log(`Bot Name: ${bot.user.username}\n`);
+  console.log(`Bot Owner: FHGDev\n`);
+  console.log(`Bot Developers:${config.Developers.join(" ")}\n`);
   console.log(`~ ${bot.guilds.array().length} Servers, ${bot.channels.array().length} Channels, and ${bot.users.array().length} Users\n`);
 }
