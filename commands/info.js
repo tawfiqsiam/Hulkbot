46dfae5b498ebfa0885bcffe65056d67b8357b9c
FHGDev <gideon.lingle1@gmail.com>
2018-10-18 10:39:16 -0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,7 @@ const pkg = require('../package.json')
 
 module.exports.run = (bot, message, args, discord) => {
   const embed = new discord.RichEmbed()  
-  .addField('Hi!' `Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot running on NodeJS Version 8.`)
+  .addField("Hi!" `Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot running on NodeJS Version 8.`)
   .addField(`When was I born?`,`My create date is January of 2017`)
   .addField(`Version?`,`I'm currently running on Discord.js version 12.0.0 which utilizes the latest version of Discord.js, an *unofficial* Discord library by hydrabolt.`)
   .addField(`Creators`,`The developers of my source code are <@242734840829575169> and <@140487710727995392>.`)
