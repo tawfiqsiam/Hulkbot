a926aa0cd19bff77bc8e3e9ce2b25415d8b815d4
FHGDev <fhgdev@tutanota.com>
2018-10-21 12:12:34 -0500

    Update supportannounce.js

--- commands/supportannounce.js
+++ commands/supportannounce.js
@@ -4,20 +4,14 @@ module.exports.run = (bot, message, args, discord) => {
     const own = guild.owner
     const msg = args.join(" ");
     if (!supportguild.available) return;
-    
-    if (!message.member.roles.get('501815598662287382') || message.author.id !== process.env.oid) {
-      return message.channel.send(`Error\nYou do not have the 'Support Team' role on Hulkbot Support Server.`)
-    } else {
-      const sembed = new discord.RichEmbed()
+    const sembed = new discord.RichEmbed()
       .addField(`Support Announcement`, `This is an important announcement from the support team.`)
       .addField(`Heyo! This is an official announcement from the support team.`, `Announcement:\n**${msg}**`)
-      .setFooter("Official Announcement")
+      .setFooter("Announcement | This system will *not* be spammed.")
       .setTimestamp()
       .setColor("GREEN")
-      guild.channels.forEach((id, channel) => {
-        // SOON
-      })
-    }
+    
+    own.send({embed: sembed})
   })
 }
 
