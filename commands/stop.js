4d356a3e5be4e4642c9743d44a3633f1cb217944
FHGDev <gideon.lingle1@gmail.com>
2018-10-24 19:25:02 -0700

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,14 +1,13 @@
 module.exports.run = (bot, message, args) => {
   var queue = require('./play.js').queue
-  const vc = message.member.voiceChannel
-  if (!vc) return message.channel.send("You need to be in a voice channel to use this command.");
+  const vc = message.guild.me.voiceChannel()
+  if (!vc) return message.channel.send("I am not playing music.");
   const discord = require('discord.js');
   const em = new discord.RichEmbed()
-  .addField("Stop Playing", `I stopped playing music in ${vc.name}, and cleared the queue.`)
+  .addField("Stop Playing", `I stopped playing music in ${vc.name}.`)
   .setTimestamp()
   .setColor("RED");
   vc.leave();
-  queue = [];
   message.channel.send({embed: em});
 }
 
