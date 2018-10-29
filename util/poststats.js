a83a06bac6d3e4430d100edd341ccb9f780100cb
FHGDev <gideon.lingle1@gmail.com>
2018-10-29 11:49:13 -0700

    Update poststats.js

--- util/poststats.js
+++ util/poststats.js
@@ -2,9 +2,9 @@ const snekfetch = require('snekfetch')
  
 module.exports = (bot) => {
  setInterval(() => {
-  snekfetch.post(`https://discordbots.org/api/bots/stats`)
+  snekfetch.post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
     .set('Authorization', process.env.tok)
-    .send({ server_count: bot.guilds.array().length })
+    .send({ server_count: bot.guilds.size })
     .then(() => console.log('Updated discordbots.org stats.'))
     .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
 }, 3600000)
