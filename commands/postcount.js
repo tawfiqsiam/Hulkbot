a5b0a7d8380129068aaf6d93c83d46636f95a1ac
FHGDev <gideon.lingle1@gmail.com>
2018-11-11 17:14:59 -0500

    Update postcount.js

--- commands/postcount.js
+++ commands/postcount.js
@@ -1,11 +1,11 @@
 module.exports.run = (bot, message, args) => {
   if (!message.author.id == process.env.oid) return message.channel.send("Nope!");
   const msg = "Posted my server count to DBL."
-  require('snekfetch').post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
+  require('snekfetch').post(`https://discordbots.org/api/bots/stats`)
     .set('Authorization', process.env.tok)
     .send({ server_count: bot.guilds.size })
     .then(() => console.log('Updated discordbots.org stats.'))
-    .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
+    .catch(err => console.error(`Whoops something went wrong: ${err}`));
     message.channel.send(msg)
 }
 
