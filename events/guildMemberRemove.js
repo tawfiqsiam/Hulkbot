a462207a2d908dcc60bb140b9f45634d44a3eee8
FHGDev <gideon.lingle1@gmail.com>
2018-11-11 07:00:09 -0500

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,6 @@
 module.exports = (bot, member) => {
     console.log(`${member.displayName} just left ${member.guild.name}.`)
-    let guildids = ["318532861638737931", "264445053596991498",  "110373943822540800", "349661783612719119"]
+    let guildids = ["318532861638737931", "264445053596991498",  "110373943822540800", "349661783612719119", "442067917979385859"]
     
     if (guildids.includes(member.guild.id)) {
         return;
