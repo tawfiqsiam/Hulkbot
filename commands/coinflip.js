b322d687eab012a95e6b4178049d8ad419fef9a3
FHGDev <gideon.lingle1@gmail.com>
2018-12-06 14:35:51 -0600

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -3,20 +3,19 @@ module.exports.run = (bot, message, args, discord) => {
   if (result) {
     let embed = new discord.RichEmbed()
     .setTitle(`${bot.user.username} Coinflip`)
-    .setThumbnail(bot.user.avatarURL)
+    .setThumbnail(`https://media3.giphy.com/media/mA51FMHGo3BDi/giphy.gif`)
     .setDescription(`Welp! The coin landed on heads! You win.`)
     .setColor(`GREEN`)
-    .setImage(`https://media3.giphy.com/media/mA51FMHGo3BDi/giphy.gif`)
-    .setFooter(new Date())
-    message.channel.send(embed)
+    .setTimestamp()
+    message.channel.send({embed: embed}))
   } else {
     let em = new discord.RichEmbed()
     .setTitle(`${bot.user.username} Coinflip`)
+    .setThumbnail(`https://media3.giphy.com/media/mA51FMHGo3BDi/giphy.gif`)
     .setDescription(`Welp! The coin landed on tails. You lose.`)
     .setColor(`RED`)
-    .setImage(`https://media3.giphy.com/media/mA51FMHGo3BDi/giphy.gif`)
-    .setFooter(new Date())
-    message.channel.send(em)
+    .setTimestamp()
+    message.channel.send({embed: em})
   }
 }
 
@@ -24,10 +23,4 @@ module.exports.help = {
   name: "coinflip",
   usage: ``,
   information: "Do a coinflip!"
-}
-
-module.exports.settings = {
-  permission: "All",
-  deleteresponder: true,
-  category: "Fun"
 }
