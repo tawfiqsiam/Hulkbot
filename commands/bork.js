0a04b849d195b9e3826a2df6cc8653759619f203
FHGDev <gideon.lingle1@gmail.com>
2018-12-06 14:40:24 -0600

    Update bork.js

--- commands/bork.js
+++ commands/bork.js
@@ -3,9 +3,8 @@ module.exports.run = (bot, message, args, discord) => {
 	if (!member) return message.channel.send("You need to mention someone.");
 	let embed = new discord.RichEmbed()
 	.setTitle(`BORK!`)
-	.setImage(`https://orig00.deviantart.net/3bd3/f/2015/141/0/5/bork_by_chiibe-d8u6uv3.gif`)
 	.setDescription(`( ͡° ͜ʖ ͡°) <@${member.id}> was borked by <@${message.author.id}>`)
-	.setThumbnail(bot.user.avatarURL)
+	.setThumbnail(`https://orig00.deviantart.net/3bd3/f/2015/141/0/5/bork_by_chiibe-d8u6uv3.gif`)
 	.setColor(`RANDOM`)
 	message.channel.send({ embed });
 }
