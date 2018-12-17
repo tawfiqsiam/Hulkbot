98a729102644788eee89745470856ac18959cc84
FHGDev <gideon.lingle1@gmail.com>
2018-12-17 16:09:11 -0600

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -7,6 +7,17 @@ async function runCmd(bot, message, args) {
   if (prefix.length > 3) return message.channel.send("My prefix cannot be over 3 characters long.");
   
   gdata.findOne({guildId: message.guild.id}, (err,data) => {
+    if (data == null) {
+      const newG = new gdata({
+        guildId: message.guild.id,
+        prefix: prefix,
+        isPremium: false,
+        welcome: null,
+        modlog: null,
+        isBL: false
+      })
+      newg.save()
+    }
     const em = new (require('discord.js').RichEmbed)()
     .setTitle("Hulkbot Prefix Changer")
     .setDescription(`I changed my prefix for this guild!`)
