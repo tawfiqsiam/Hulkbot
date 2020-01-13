6f5b537141b9665ba371df3921bcee187d21871f
tawfiqsiam <toto.siam999@gmail.com>
2020-01-13 15:31:19 +0200

    Update joinserver.js

--- commands/joinserver.js
+++ commands/joinserver.js
@@ -1,5 +1,5 @@
 module.exports.run = (bot, message, args) => {
-  message.channel.send("Use this to join the help server: https://discord.gg/XvMA2rJ")
+  message.channel.send("Use this to join the help server: https://discord.gg/G77DG59")
 }
 
 module.exports.help = {
