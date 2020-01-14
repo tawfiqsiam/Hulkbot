0a92847bbe12b9d87b062cdbc5952352eab9d71f
tawfiqsiam <toto.siam999@gmail.com>
2020-01-14 14:25:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,9 @@ bot.invite = "https://discord.gg/G77DG59"
 // Gather commands
 bot.commands = new discord.Collection();
 
-
+process.on('unhandledRejection', (reason, promise) => {
+  console.log(new Error(reason))
+})
 
 require('fs').readdir("./commands/", (err, files) => {
   console.log("Loading commands...");
